6181cc02fe7b7adab86df5e1346cbd2a2fa8c6da
aelassas <akram.elassas@gmail.com>
2020-05-09 11:54:10 +0000

    Updated Wexflow.Scripts.Oracle

--- src/net/Wexflow.Scripts.Oracle/Program.cs
+++ src/net/Wexflow.Scripts.Oracle/Program.cs
@@ -12,6 +12,8 @@ namespace Wexflow.Scripts.Oracle
             {
                 Db db = new Db(ConfigurationManager.AppSettings["connectionString"]);
                 Core.Helper.InsertWorkflowsAndUser(db);
+                Core.Helper.InsertRecords(db);
+                db.Dispose();
             }
             catch (Exception e)
             {
