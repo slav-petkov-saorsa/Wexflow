930f20bf11947e9d872392a7b11d1811b3bdc73a
aelassas <akram.elassas@gmail.com>
2020-05-09 11:50:44 +0000

    Updated Wexflow.Scripts.Firebird

--- src/net/Wexflow.Scripts.Firebird/Program.cs
+++ src/net/Wexflow.Scripts.Firebird/Program.cs
@@ -12,6 +12,8 @@ namespace Wexflow.Scripts.Firebird
             {
                 Db db = new Db(ConfigurationManager.AppSettings["connectionString"]);
                 Core.Helper.InsertWorkflowsAndUser(db);
+                Core.Helper.InsertRecords(db);
+                db.Dispose();
             }
             catch (Exception e)
             {
