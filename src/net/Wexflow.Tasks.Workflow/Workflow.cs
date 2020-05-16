c190eeb8d46894a3c12b0380eb521004fca2b3df
aelassas <akram.elassas@gmail.com>
2020-05-16 05:49:12 +0000

    Fixed some issues in Workflow task (#239)

--- src/net/Wexflow.Tasks.Workflow/Workflow.cs
+++ src/net/Wexflow.Tasks.Workflow/Workflow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
 using Wexflow.Core;
 using Wexflow.Core.Service.Client;
@@ -43,96 +44,149 @@ namespace Wexflow.Tasks.Workflow
             bool atLeastOneSucceed = false;
             foreach (var id in WorkflowIds)
             {
-                WexflowServiceClient client = new WexflowServiceClient(WexflowWebServiceUri);
-                WorkflowInfo wfInfo = client.GetWorkflow(Username, Password, id);
-                switch (Action)
+                try
                 {
-                    case WorkflowAction.Start:
-                        if (wfInfo.IsRunning)
-                        {
-                            success = false;
-                            ErrorFormat("Can't start the workflow {0} because it's already running.", Workflow.Id);
-                        }
-                        else
-                        {
-                            var instanceId = client.StartWorkflow(id, Username, Password);
-                            if (Jobs.ContainsKey(id))
+                    WexflowServiceClient client = new WexflowServiceClient(WexflowWebServiceUri);
+                    WorkflowInfo wfInfo = client.GetWorkflow(Username, Password, id);
+                    switch (Action)
+                    {
+                        case WorkflowAction.Start:
+                            if (wfInfo.IsRunning)
                             {
-                                Jobs[id] = instanceId;
+                                success = false;
+                                ErrorFormat("Can't start the workflow {0} because it's already running.", id);
                             }
                             else
                             {
-                                Jobs.Add(id, instanceId);
+                                var instanceId = client.StartWorkflow(id, Username, Password);
+                                if (Jobs.ContainsKey(id))
+                                {
+                                    Jobs[id] = instanceId;
+                                }
+                                else
+                                {
+                                    Jobs.Add(id, instanceId);
+                                }
+                                InfoFormat("Workflow {0} started.", id);
+                                if (!atLeastOneSucceed) atLeastOneSucceed = true;
                             }
-                            InfoFormat("Workflow {0} started.", id);
-                            if (!atLeastOneSucceed) atLeastOneSucceed = true;
-                        }
-                        break;
-                    case WorkflowAction.Suspend:
-                        if (wfInfo.IsRunning)
-                        {
-                            client.SuspendWorkflow(id, Jobs[id], Username, Password);
-                            InfoFormat("Workflow {0} suspended.", id);
-                            if (!atLeastOneSucceed) atLeastOneSucceed = true;
-                        }
-                        else
-                        {
-                            success = false;
-                            ErrorFormat("Can't suspend the workflow {0} because it's not running.", Workflow.Id);
-                        }
-                        break;
-                    case WorkflowAction.Resume:
-                        if (wfInfo.IsPaused)
-                        {
-                            client.ResumeWorkflow(id, Jobs[id], Username, Password);
-                            InfoFormat("Workflow {0} resumed.", id);
-                            if (!atLeastOneSucceed) atLeastOneSucceed = true;
-                        }
-                        else
-                        {
-                            success = false;
-                            ErrorFormat("Can't resume the workflow {0} because it's not suspended.", Workflow.Id);
-                        }
-                        break;
-                    case WorkflowAction.Stop:
-                        if (wfInfo.IsRunning)
-                        {
-                            client.StopWorkflow(id, Jobs[id], Username, Password);
-                            InfoFormat("Workflow {0} stopped.", id);
-                            if (!atLeastOneSucceed) atLeastOneSucceed = true;
-                        }
-                        else
-                        {
-                            success = false;
-                            ErrorFormat("Can't stop the workflow {0} because it's not running.", Workflow.Id);
-                        }
-                        break;
-                    case WorkflowAction.Approve:
-                        if (wfInfo.IsApproval && wfInfo.IsWaitingForApproval)
-                        {
-                            client.ApproveWorkflow(id, Jobs[id], Username, Password);
-                            InfoFormat("Workflow {0} approved.", id);
-                            if (!atLeastOneSucceed) atLeastOneSucceed = true;
-                        }
-                        else
-                        {
-                            success = false;
-                            ErrorFormat("Can't approve the workflow {0} because it's not waiting for approval.", Workflow.Id);
-                        }
-                        break;
-                    case WorkflowAction.Reject:
-                        if (wfInfo.IsApproval && wfInfo.IsWaitingForApproval)
-                        {
-                            client.RejectWorkflow(id, Jobs[id], Username, Password);
-                            InfoFormat("Workflow {0} rejected.", id);
-                            if (!atLeastOneSucceed) atLeastOneSucceed = true;
-                        }
-                        else
-                        {
-                            success = false;
-                            ErrorFormat("Can't reject the workflow {0} because it's not waiting for approval.", Workflow.Id);
-                        }
-                        break;
+                            break;
+                        case WorkflowAction.Suspend:
+                            if (wfInfo.IsRunning)
+                            {
+                                var jobId = Workflow.WexflowEngine.GetWorkflow(id).Jobs.Select(j => j.Key).FirstOrDefault();
+                                if (jobId != null)
+                                {
+                                    client.SuspendWorkflow(id, jobId, Username, Password);
+                                    InfoFormat("Workflow {0} suspended.", id);
+                                    if (!atLeastOneSucceed) atLeastOneSucceed = true;
+                                }
+                                else
+                                {
+                                    success = false;
+                                    ErrorFormat("Can't suspend the workflow {0} because it's not running.", id);
+                                }
+                            }
+                            else
+                            {
+                                success = false;
+                                ErrorFormat("Can't suspend the workflow {0} because it's not running.", id);
+                            }
+                            break;
+                        case WorkflowAction.Resume:
+                            if (wfInfo.IsPaused)
+                            {
+                                var jobId = Workflow.WexflowEngine.GetWorkflow(id).Jobs.Select(j => j.Key).FirstOrDefault();
+                                if (jobId != null)
+                                {
+                                    client.ResumeWorkflow(id, jobId, Username, Password);
+                                    InfoFormat("Workflow {0} resumed.", id);
+                                    if (!atLeastOneSucceed) atLeastOneSucceed = true;
+                                }
+                                else
+                                {
+                                    success = false;
+                                    ErrorFormat("Can't resume the workflow {0} because it's not suspended.", id);
+                                }
+                            }
+                            else
+                            {
+                                success = false;
+                                ErrorFormat("Can't resume the workflow {0} because it's not suspended.", id);
+                            }
+                            break;
+                        case WorkflowAction.Stop:
+                            if (wfInfo.IsRunning)
+                            {
+                                var jobId = Workflow.WexflowEngine.GetWorkflow(id).Jobs.Select(j => j.Key).FirstOrDefault();
+                                if (jobId != null)
+                                {
+                                    client.StopWorkflow(id, jobId, Username, Password);
+                                    InfoFormat("Workflow {0} stopped.", id);
+                                    if (!atLeastOneSucceed) atLeastOneSucceed = true;
+                                }
+                                else
+                                {
+                                    success = false;
+                                    ErrorFormat("Can't stop the workflow {0} because it's not running.", id);
+                                }
+                            }
+                            else
+                            {
+                                success = false;
+                                ErrorFormat("Can't stop the workflow {0} because it's not running.", id);
+                            }
+                            break;
+                        case WorkflowAction.Approve:
+                            if (wfInfo.IsApproval && wfInfo.IsWaitingForApproval)
+                            {
+                                var jobId = Workflow.WexflowEngine.GetWorkflow(id).Jobs.Select(j => j.Key).FirstOrDefault();
+                                if (jobId != null)
+                                {
+                                    client.ApproveWorkflow(id, jobId, Username, Password);
+                                    InfoFormat("Workflow {0} approved.", id);
+                                    if (!atLeastOneSucceed) atLeastOneSucceed = true;
+                                }
+                                else
+                                {
+                                    success = false;
+                                    ErrorFormat("Can't approve the workflow {0} because it's not waiting for approval.", id);
+                                }
+                            }
+                            else
+                            {
+                                success = false;
+                                ErrorFormat("Can't approve the workflow {0} because it's not waiting for approval.", id);
+                            }
+                            break;
+                        case WorkflowAction.Reject:
+                            if (wfInfo.IsApproval && wfInfo.IsWaitingForApproval)
+                            {
+                                var jobId = Workflow.WexflowEngine.GetWorkflow(id).Jobs.Select(j => j.Key).FirstOrDefault();
+                                if (jobId != null)
+                                {
+                                    client.RejectWorkflow(id, jobId, Username, Password);
+                                    InfoFormat("Workflow {0} rejected.", id);
+                                    if (!atLeastOneSucceed) atLeastOneSucceed = true;
+                                }
+                                else
+                                {
+                                    success = false;
+                                    ErrorFormat("Can't reject the workflow {0} because it's not waiting for approval.", id);
+                                }
+                            }
+                            else
+                            {
+                                success = false;
+                                ErrorFormat("Can't reject the workflow {0} because it's not waiting for approval.", id);
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    success = false;
+                    ErrorFormat("An error occured while processing the workflow {0}", e, id);
                 }
             }
             Info("Task finished.");
