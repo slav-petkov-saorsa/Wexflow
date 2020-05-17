1a070c5a243c10bf9caeecf119e04bb03a8fe287
aelassas <akram.elassas@gmail.com>
2020-05-17 22:06:29 +0000

    Updated ApprovalWorkflowsCreator (#234)

--- src/netcore/Wexflow.Tasks.ApprovalWorkflowsCreator/ApprovalWorkflowsCreator.cs
+++ src/netcore/Wexflow.Tasks.ApprovalWorkflowsCreator/ApprovalWorkflowsCreator.cs
@@ -35,58 +35,60 @@ namespace Wexflow.Tasks.ApprovalWorkflowsCreator
                     Error($"Shared memory key {smKey} not found.");
                     success = false;
                 }
-
-                var recordIds = (string[])SharedMemory[smKey];
-
-                foreach (var recordId in recordIds)
+                else
                 {
-                    try
+                    var recordIds = (string[])SharedMemory[smKey];
+
+                    foreach (var recordId in recordIds)
                     {
-                        var record = Workflow.Database.GetRecord(recordId);
-                        var workflowId = Workflow.WexflowEngine.Workflows.Select(w => w.Id).Max() + 1;
-                        var workflowName = $"Workflow_ApproveRecord_{SecurityElement.Escape(Approver)}_{SecurityElement.Escape(record.Name)}";
-
-                        var xml = $"<Workflow xmlns='urn:wexflow-schema' id='{workflowId}' name='{workflowName}' description='{workflowName}'>\r\n"
-                                + "	 <Settings>\r\n"
-                                + "    <Setting name='launchType' value='trigger' />\r\n"
-                                + "    <Setting name='enabled' value='true' />\r\n"
-                                + "    <Setting name='approval' value='true' />\r\n"
-                                + "    <Setting name='enableParallelJobs' value='true' />\r\n"
-                                + "	  </Settings>\r\n"
-                                + "	 <LocalVariables />\r\n"
-                                + "	 <Tasks>\r\n"
-                               + $"    <Task id='1' name='ApproveRecord' description='Approving record {SecurityElement.Escape(record.Name)}' enabled='true'>\r\n"
-                               + $"      <Setting name='record' value='{SecurityElement.Escape(recordId)}' />\r\n"
-                               + $"      <Setting name='assignedTo' value='{SecurityElement.Escape(AssignedTo)}' />\r\n"
-                                + "    </Task>\r\n"
-                                + "  </Tasks>\r\n"
-                                + "</Workflow>\r\n";
-
-                        var approver = Workflow.WexflowEngine.GetUser(Approver);
-                        var worflowId = Workflow.WexflowEngine.SaveWorkflow(approver.GetDbId(), approver.UserProfile, xml, false);
-
-                        if (worflowId != "-1")
+                        try
                         {
-                            var workflow = Workflow.WexflowEngine.GetWorkflow(workflowId);
-                            workflow.StartAsync(Approver);
-                            Info($"Approval Workflow of the record {recordId} - {record.Name} created and started successfully.");
+                            var record = Workflow.Database.GetRecord(recordId);
+                            var workflowId = Workflow.WexflowEngine.Workflows.Select(w => w.Id).Max() + 1;
+                            var workflowName = $"Workflow_ApproveRecord_{SecurityElement.Escape(Approver)}_{SecurityElement.Escape(record.Name)}";
+
+                            var xml = $"<Workflow xmlns='urn:wexflow-schema' id='{workflowId}' name='{workflowName}' description='{workflowName}'>\r\n"
+                                    + "	 <Settings>\r\n"
+                                    + "    <Setting name='launchType' value='trigger' />\r\n"
+                                    + "    <Setting name='enabled' value='true' />\r\n"
+                                    + "    <Setting name='approval' value='true' />\r\n"
+                                    + "    <Setting name='enableParallelJobs' value='true' />\r\n"
+                                    + "	  </Settings>\r\n"
+                                    + "	 <LocalVariables />\r\n"
+                                    + "	 <Tasks>\r\n"
+                                   + $"    <Task id='1' name='ApproveRecord' description='Approving record {SecurityElement.Escape(record.Name)}' enabled='true'>\r\n"
+                                   + $"      <Setting name='record' value='{SecurityElement.Escape(recordId)}' />\r\n"
+                                   + $"      <Setting name='assignedTo' value='{SecurityElement.Escape(AssignedTo)}' />\r\n"
+                                    + "    </Task>\r\n"
+                                    + "  </Tasks>\r\n"
+                                    + "</Workflow>\r\n";
+
+                            var approver = Workflow.WexflowEngine.GetUser(Approver);
+                            var worflowId = Workflow.WexflowEngine.SaveWorkflow(approver.GetDbId(), approver.UserProfile, xml, false);
+
+                            if (worflowId != "-1")
+                            {
+                                var workflow = Workflow.WexflowEngine.GetWorkflow(workflowId);
+                                workflow.StartAsync(Approver);
+                                Info($"Approval Workflow of the record {recordId} - {record.Name} created and started successfully.");
+                            }
+                            else
+                            {
+                                Error($"An error occured while creating the approval workflow of the record {recordId} - {record.Name}.");
+                                success = false;
+                            }
+
+                            if (success && !atLeastOneSuccess) atLeastOneSuccess = true;
                         }
-                        else
+                        catch (ThreadAbortException)
                         {
-                            Error($"An error occured while creating the approval workflow of the record {recordId} - {record.Name}.");
+                            throw;
+                        }
+                        catch (Exception e)
+                        {
+                            ErrorFormat("An error occured while creating the approval workflow for the record {0}.", e, recordId);
                             success = false;
                         }
-
-                        if (success && !atLeastOneSuccess) atLeastOneSuccess = true;
-                    }
-                    catch (ThreadAbortException)
-                    {
-                        throw;
-                    }
-                    catch (Exception e)
-                    {
-                        ErrorFormat("An error occured while creating the approval workflow for the record {0}.", e, recordId);
-                        success = false;
                     }
                 }
             }
